c1629aac134b9fe271b85cbea366e34cfe0ec85b
wangwei <1521491171@qq.com>
2023-03-07 20:40:24 +0800

    testtest

---
+++ src/main/java/com/mystu/Spring26AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.mystu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring26AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring26AdminClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mystu/Spring26AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring26AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
